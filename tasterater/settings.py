71e161548b2e4efb716cd9cb3f3766460986ddc4
Qilian(Daisy) <qiliansong@gmail.com>
2024-06-04 16:06:13 -0700

    add dynamic nav bar

--- tasterater/settings.py
+++ tasterater/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/'
 
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
