7697377a528b1e1f607b00bc5639a1091e770ef4
maniac-b8 <matthewdixon2539@gmail.com>
2024-06-05 09:42:13 -0700

    add review model and functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/signup/', views.signup, name='signup'),  
+    path('accounts/signup/', views.signup, name='signup'), 
+    path('add_review/<str:yelp_id>/', views.add_review, name='add_review'), 
+    path('edit_review/<int:review_id>/', views.edit_review, name='edit_review'), 
+    path('delete_review/<int:review_id>/', views.delete_review, name='delete_review'), 
 ]
